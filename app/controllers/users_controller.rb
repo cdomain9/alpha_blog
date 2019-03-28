2a7b69db12f7bde8c454865a60630f01a677374a
cdomain <christopherdomain9@gmail.com>
2019-03-28 13:36:12 -0500

    added admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -11,8 +14,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      session[:user_id] = @user.id
   	  flash[:success] = "Welcome to the alpha blog #{@user.username}"
-  	  redirect_to articles_path
+  	  redirect_to user_path(@user)
   	else
   	  render 'new'
   	end
@@ -34,6 +38,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -41,5 +52,19 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user and !current_user.admin?
+      flash[:danger] = "You can only edit you're own account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
+      redirect_to root_path
+    end
   end
 end
